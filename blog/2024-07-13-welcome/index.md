6bfa68cc3ac9b327e183e835ef2e71739c763201
spicydev <vivek@mirchi.dev>
2024-07-16 15:12:02 -0400

    first commit

---
+++ blog/2024-07-13-welcome/index.md
@@ -0,0 +1,12 @@
+---
+slug: welcome
+title: Welcome
+authors: [spicydev]
+tags: [facebook, hello, docusaurus]
+---
+
+[Docusaurus blogging features](https://docusaurus.io/docs/blog) are powered by the [blog plugin](https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog).
+
+Today is the first time I was able to get around scafolding this Blog and bring it live to you, my readers!
+
+This Blog will be about my learnings of software engineering and involving mainly the concepts and a bit of sample code.
---
+++ docusaurus.config.ts
@@ -0,0 +1,107 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Spicy Developer',
+  tagline: 'Unlock overclocking your mind with spicy food',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://mirchi.dev',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'spicydev', // Usually your GitHub org/user name.
+  projectName: 'mirchi-dev', // Usually your repo name.
+
+  onBrokenLinks: 'log',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Spicy Dev',
+      logo: {
+        alt: 'Mirchi Logo',
+        src: 'img/mirchi-logo-black.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/aghome/#', label: 'AdGuard', position: 'left'},
+        {
+          href: 'https://github.com/spicydev',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          label: 'Docs',
+          to: '/docs/intro',
+        },
+        {
+          label: 'Blog',
+          to: '/blog',
+        },
+        {
+          label: 'AdGuard Home',
+          href: '/aghome/#',
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} mirchi.dev. Built with Docusaurus. Secured with LetsEncrypt.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
